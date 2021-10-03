853b886d6a663fedc786b0d7471943dfc1190ce3
Reinaldo Dos Santos <reinalddos@gmail.com>
2021-10-03 19:31:23 +0200

    SSH key

--- CRONJOB.R
+++ CRONJOB.R
@@ -2,12 +2,15 @@ setwd(dir = "~/R/GEODES/")
 ssh_auth_sock <- system("echo $SSH_AUTH_SOCK", intern = TRUE)
 Sys.setenv(SSH_AUTH_SOCK = ssh_auth_sock)
 
-git2r::pull()
+repo = git2r::repository()
+SSH = git2r::cred_ssh_key()
+
+git2r::pull(repo = repo, credentials = SSH)
 
 source(file = "The plan.R", echo = TRUE)
 rmarkdown::render(input = "Description.Rmd", 
                   output_file = "Description.html")
 
-git2r::add(path = "Description.html")
-git2r::commit(message = "update HTML")
-git2r::push()
+git2r::add(repo = repo, path = "Description.html")
+git2r::commit(repo = repo, message = "update HTML")
+git2r::push(credentials = SSH, object = repo)
