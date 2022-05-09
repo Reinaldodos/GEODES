a5e36d5a0d40fa80e88b9d8c54643265046f48d4
Reinaldo Dos Santos <reinalddos@gmail.com>
2022-05-09 13:11:03 +0200

    rien à voir

--- CRONJOB.R
+++ CRONJOB.R
@@ -1,7 +1,7 @@
 setwd(dir = "~/R/GEODES/")
 ssh_auth_sock <- system("echo $SSH_AUTH_SOCK", intern = TRUE)
 Sys.setenv(SSH_AUTH_SOCK = ssh_auth_sock)
-Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio/bin/pandoc")
+Sys.setenv(RSTUDIO_PANDOC = "/usr/lib/rstudio/bin/pandoc")
 
 require(git2r)
 require(rmarkdown)
@@ -14,9 +14,10 @@ source(file = "The plan.R", echo = TRUE)
 drake::clean(list = c("data", "output_GEODES"))
 drake::make(plan = the_plan)
 
-rmarkdown::render(input = "Description.Rmd", 
+rmarkdown::render(input = "Description.Rmd",
                   output_file = "Description.html")
 
 git2r::add(repo = repo, path = "Description.html")
 git2r::commit(repo = repo, message = "update HTML")
 git2r::push(credentials = SSH, object = repo)
+
